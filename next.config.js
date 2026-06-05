46d96a98aadc948c1f9a549e2afa6326d1612227
Himaz Developer <himaz@properties.com>
2026-06-05 20:10:37 +0100

    Prepare for Cloudflare Pages deploy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/himaz_properties',
-  assetPrefix: '/himaz_properties',
   images: {
     unoptimized: true,
     formats: ['image/webp'],
